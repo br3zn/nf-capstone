fe86af48dbf39f38714d0fcc52c982f83d65ae70
brezn <br3zn@users.noreply.github.com>
2022-02-15 15:26:06 +0100

    [feature] add temporary index page with Logo component

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,31 @@
 import Head from "next/head";
+import Logo from "../components/Logo";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Next.js Template</title>
+        <title>Bubatz - educational cannabis app</title>
         <meta
           name="description"
-          content="Next.js Template powered with tailwind and apollo/client"
+          content="Bubatz is a educational app about cannabis and everything around it."
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={`flex h-screen items-center justify-center`}>
-        <h1 className={`text-4xl`}>
-          This is a{" "}
-          <a
-            className={`text-blue-500 hover:underline`}
-            href="https://github.com/br3zn/project-template"
-          >
-            Next.js Template!
-          </a>
+      <main
+        className={`flex min-h-screen w-screen flex-col items-center justify-center gap-6 bg-slate-900`}
+      >
+        <Logo />
+        <h1 className={`text-center text-4xl text-gray-200`}>
+          Under active development.{" "}
         </h1>
+        <a
+          className={`text-blue-500 hover:underline`}
+          href="https://github.com/br3zn/nf-capstone"
+        >
+          GitHub repository
+        </a>
       </main>
     </>
   );
