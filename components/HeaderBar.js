c5d4bd45ccb23729834f6a71efc928e97d5ebd54
brezn <br3zn@users.noreply.github.com>
2022-02-16 15:48:57 +0100

    [chore] refactored existing styles

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -1,11 +1,16 @@
 import LogoIcon from "./LogoIcon";
 import LogoText from "./LogoText";
+import Link from "next/link";
 
 export default function HeaderBar(props) {
   return (
-    <div className="sticky top-0 left-0 flex min-h-fit w-screen items-center justify-around gap-4 bg-white/30 py-4 px-8 backdrop-blur-md backdrop-opacity-80">
-      <LogoIcon size={"sm"} />
-      <LogoText size={"sm"} />
+    <div className="sticky top-0 left-0 z-30 flex min-h-fit w-screen items-center justify-center gap-4 bg-white/80 backdrop-blur dark:bg-black/60">
+      <Link href={"/"} passHref>
+        <div className={`flex cursor-pointer gap-4 py-4`}>
+          <LogoIcon size={"sm"} />
+          <LogoText size={"sm"} />
+        </div>
+      </Link>
     </div>
   );
 }
