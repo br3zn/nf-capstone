776e99b0b9a88d02724aab3f99ea1c605859602e
brezn <br3zn@users.noreply.github.com>
2022-02-16 19:40:44 +0100

    [feature] add custom heading depending on pathname

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -1,14 +1,13 @@
 import LogoIcon from "./LogoIcon";
-import LogoText from "./LogoText";
 import Link from "next/link";
 
-export default function HeaderBar(props) {
+export default function HeaderBar({ children }) {
   return (
     <div className="sticky top-0 left-0 z-30 flex min-h-fit w-screen items-center justify-center gap-4 bg-white/80 backdrop-blur dark:bg-black/60">
       <Link href={"/"} passHref>
         <div className={`flex cursor-pointer gap-4 py-4`}>
           <LogoIcon size={"sm"} />
-          <LogoText size={"sm"} />
+          {children}
         </div>
       </Link>
     </div>
--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,22 @@
 import HeaderBar from "./HeaderBar";
+import { useRouter } from "next/router";
+import LogoText from "./LogoText";
+import HeadingStrain from "./Heading/Strain";
 
 export default function Layout({ children }) {
+  const router = useRouter();
+  const currentPath = router.pathname;
+  const heading = () => {
+    if (currentPath === "/strains") {
+      return <HeadingStrain size={"sm"} />;
+    } else {
+      return <LogoText size={"sm"} />;
+    }
+  };
+
   return (
     <div className={`min-h-screen bg-slate-50 dark:bg-slate-900`}>
-      <HeaderBar />
+      <HeaderBar>{heading()}</HeaderBar>
       <main>{children}</main>
     </div>
   );
