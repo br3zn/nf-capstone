636ed8a120137ab9d03f1c6b007d3e3de7ada864
brezn <br3zn@users.noreply.github.com>
2022-02-18 17:27:58 +0100

    [chore] add schema and resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,5 +3,8 @@ module.exports = {
     allStrains: async (_, { skip, take }, { dataSources }) => {
       return dataSources.leaflyAPI.getAllStrains(skip, take);
     },
+    getTerps: async (_, __, { dataSources }) => {
+      return dataSources.bubatzDB.getTerps();
+    },
   },
 };
--- src/schemas.js
+++ src/schemas.js
@@ -6,16 +6,22 @@ const typeDefs = gql`
     name: String!
     terpTop: String!
     thc: Int
-    cbd: Int
     flowerSvg: String
     terps: [Terpene]
   }
   type Terpene {
     name: String
     score: Float
+    description: String
+  }
+  type TerpInfo {
+    id: Int!
+    name: String
+    description: String
   }
   type Query {
     allStrains(skip: Int!, take: Int!): [Strain]!
+    getTerps: [TerpInfo]
   }
 `;
 module.exports = typeDefs;
