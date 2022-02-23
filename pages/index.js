5fdd55c8a272d8d68e85f4122e1ab7a4096aace4
brezn <br3zn@users.noreply.github.com>
2022-02-23 18:44:58 +0100

    [feature] add nav list to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import LogoIcon from "../components/LogoIcon";
+import Link from "next/link";
+import NavMenu from "../components/NavMenu";
 
 export default function Home() {
   return (
@@ -12,17 +13,38 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={`flex flex-col items-center justify-center gap-6 pt-8`}>
-        <LogoIcon size={"lg"} />
-        <h2 className={`text-center text-4xl text-gray-800 dark:text-gray-200`}>
-          Under development.{" "}
-        </h2>
-        <a
-          className={`text-blue-500 hover:underline`}
-          href="https://github.com/br3zn/nf-capstone"
+      <NavMenu />
+      <div
+        className={`flex h-full flex-col items-center justify-evenly gap-6 pt-8 dark:text-slate-400`}
+      >
+        <h2
+          className={`text-center text-4xl font-black uppercase tracking-wide text-gray-800 dark:text-gray-300`}
         >
-          GitHub repository
-        </a>
+          What is{" "}
+          <span
+            className={`relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-2 before:bg-red-500 `}
+          >
+            <span className={`relative`}>Bubatz?</span>
+          </span>
+        </h2>
+        <div className={`space-y-8 px-4 text-lg`}>
+          <p className={`max-w-md`}>
+            This app is about cannabis and the benefits of the different
+            terpenes included. The naming comes from the german slang word for a
+            joint.
+          </p>
+          <p className={`max-w-md text-right`}>
+            You can get an overview of{" "}
+            <Link href={`/strains`}>
+              <a className={`text-slate-100`}>strains</a>
+            </Link>{" "}
+            or read more about the different{" "}
+            <Link href={`/terps`}>
+              <a className={`text-slate-100`}>terpenes</a>
+            </Link>{" "}
+            included in cannabis.
+          </p>
+        </div>
       </div>
     </>
   );
