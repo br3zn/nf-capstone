1a0259f67315f7f1bb4abcf51807cd038ac89219
brezn <br3zn@users.noreply.github.com>
2022-02-23 23:52:28 +0100

    [chore] fix color overlay being solid and removed min max values from PolarChartConfig.js

--- components/PolarChartConfig.js
+++ components/PolarChartConfig.js
@@ -1,12 +1,9 @@
 export const Config = {
-  //   animate in
   animation: {
     duration: 1,
   },
   scales: {
     r: {
-      max: 0.5,
-      min: 0,
       ticks: {
         stepSize: 0.1,
       },
