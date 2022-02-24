022ccf09b651d87b2793a33bbe10612ed2b6f304
brezn <br3zn@users.noreply.github.com>
2022-02-24 17:39:39 +0100

    [chore] add Head element

--- pages/terps/index.js
+++ pages/terps/index.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import Head from "next/head";
 
 const ALL_TERPS = gql`
   query GetAllTerps {
@@ -17,23 +18,36 @@ export default function TerpsPage() {
   if (loading) return <h3>Loading</h3>;
   const { getAllTerps } = data;
   return (
-    <div
-      className={`ml-8 mt-4 flex flex-col items-start justify-center gap-14 dark:text-slate-400`}
-    >
-      {getAllTerps.map(terp => (
-        <details className={``} key={terp.id}>
-          <summary
-            className={`cursor-pointer text-2xl text-slate-300 before:pl-3`}
-          >
-            <span
-              className={`relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-2 before:bg-red-500 `}
-            >
-              <span className={`relative`}>{terp.name}</span>
-            </span>
-          </summary>
-          <p className={``}>{terp.description}</p>
-        </details>
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Terps | Bubatz</title>
+        <meta
+          name="description"
+          content="Bubatz is a educational app about cannabis and everything around it."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        className={`ml-8 mt-4 flex flex-col items-center justify-center gap-14 dark:text-slate-400`}
+      >
+        <h2>Terpene Information</h2>
+        {getAllTerps.map(terp => (
+          <div key={terp.id} className={`w-80 md:w-96`}>
+            <details className={``}>
+              <summary
+                className={`cursor-pointer text-2xl text-slate-300 before:pl-3`}
+              >
+                <span
+                  className={`relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-2 before:bg-red-500 `}
+                >
+                  <span className={`relative`}>{terp.name}</span>
+                </span>
+              </summary>
+              <p className={``}>{terp.description}</p>
+            </details>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
