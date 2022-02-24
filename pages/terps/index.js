7f97e4f76ea53fe663d35d7430c2579772ab233d
brezn <br3zn@users.noreply.github.com>
2022-02-24 17:05:37 +0100

    [chore] basic styling for terps overview

--- pages/terps/index.js
+++ pages/terps/index.js
@@ -17,12 +17,22 @@ export default function TerpsPage() {
   if (loading) return <h3>Loading</h3>;
   const { getAllTerps } = data;
   return (
-    <div className={`dark:text-slate-400`}>
+    <div
+      className={`ml-8 mt-4 flex flex-col items-start justify-center gap-14 dark:text-slate-400`}
+    >
       {getAllTerps.map(terp => (
-        <div key={terp.id} className={``}>
-          <h3 className={`dark:text-slate-300`}>{terp.name}</h3>
-          <p>{terp.description}</p>
-        </div>
+        <details className={``} key={terp.id}>
+          <summary
+            className={`cursor-pointer text-2xl text-slate-300 before:pl-3`}
+          >
+            <span
+              className={`relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-2 before:bg-red-500 `}
+            >
+              <span className={`relative`}>{terp.name}</span>
+            </span>
+          </summary>
+          <p className={``}>{terp.description}</p>
+        </details>
       ))}
     </div>
   );
